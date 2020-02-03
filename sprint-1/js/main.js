928014f9ece3ecf5899eed5a66f4723f5540abd1
Rafay Ali <Raf@rafays-mbp.lan>
2020-02-02 22:27:41 -0500

    comments made responsive for tablet git add -A git

--- sprint-1/js/main.js
+++ sprint-1/js/main.js
@@ -6,8 +6,8 @@ commentsSection.appendChild(commentsOutputs);
 
 let arrUserNames = ['Daniil Molodkov','Rakesh Mistry','Joshua Taguicana'];
 let arrComments = ["Wow! This band is great, but their website is even better! I love the animation on the navigation links!","I came for the music but I stayed for the responsive design. It even looks good in between breakpoints!","There\'s no way I\'m giving this site anything less than a 100%. Whoever made this knows what they\'re doing!"];
-let arrDates = ['12/18/2018','12/12/2018','11/15/2018'];
-let arrSrcs = ["./assets/images/comment1.jpeg", "./assets/images/comment2.jpeg", "./assets/images/comment3.jpeg"];
+let arrDates = ['12/18/2018','12/12/2018','11/15/2018','Now'];
+let arrSrcs = ["./assets/images/comment1.jpeg", "./assets/images/comment2.jpeg", "./assets/images/comment3.jpeg","./assets/images/profile.jpg"];
 
 const createCard = function (arrUserNames,arrComments, arrDates, arrSrcs) {
     const commentsCard = document.createElement('div');
@@ -43,6 +43,29 @@ const createCard = function (arrUserNames,arrComments, arrDates, arrSrcs) {
 for (var i = 0; i <= arrUserNames.length-1; i++) {
     createCard(arrUserNames,arrComments,arrDates,arrSrcs);
 }
+
+const commentButton = document.querySelector('.comments__button');
+commentButton.addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+commentButton.onclick = function clickHandler () {
+    var username = document.querySelector(".comments__username").value;
+    var comment = document.querySelector(".comments__text").value;
+    
+    arrUserNames.unshift(username);
+    arrComments.unshift(comment);
+
+    console.log(arrUserNames);
+
+
+
+    // for (var i = 0; i <= arrUserNames.length-1; i++) {
+    //     createCard(arrUserNames,arrComments,arrDates,arrSrcs);
+    // }
+}
+
+
 
 
 
