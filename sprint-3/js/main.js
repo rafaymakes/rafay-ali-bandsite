6b5cdab1d8f8a3921594f33b3c34597bd100fd58
Rafay Ali <Raf@Rafays-MBP.localdomain>
2020-02-07 15:27:06 -0500

    Shows section properly getting and appending data from API

--- sprint-3/js/main.js
+++ sprint-3/js/main.js
@@ -1,5 +1,7 @@
+//grabbing the comments section in the html file and storing it in a variable
 const commentsSection = document.querySelector(".comments");
 
+//creating and appending an outputs section
 const commentsOutputs = document.createElement('div');
 commentsOutputs.classList.add('comments__outputs');
 commentsSection.appendChild(commentsOutputs);
@@ -9,15 +11,13 @@ let arrComments = ["Wow! This band is great, but their website is even better! I
 let arrDates = ['12/18/2018','12/12/2018','11/15/2018'];
 let arrSrcs = ["./assets/images/comment1.jpeg", "./assets/images/comment2.jpeg", "./assets/images/comment3.jpeg"];
 
-const createCard = function (userNames, comments, dates, sources) {
+//creating a function for creating each individual comment 'card' and filling it with the data from the web API
+const createCard = function (userNames, comments, dates) {
+
+    //first creating and appending all the needed elements, plus the classes used to style them
     const commentsCard = document.createElement('div');
     commentsCard.classList.add('comments__card');
     commentsOutputs.appendChild(commentsCard);
-
-    const userPicture = document.createElement('img');
-    userPicture.classList.add('comments__outpicture');
-    userPicture.src = (sources);
-    commentsCard.appendChild(userPicture);
 
     const nameDate = document.createElement('div');
     nameDate.classList.add('comments__namedate');
@@ -39,19 +39,31 @@ const createCard = function (userNames, comments, dates, sources) {
     commentsCard.appendChild(commentText);
 }
 
+//a for loop used for inputting the data from web API. 
+//It iterates through each of the indexes values at the same time, creating one comment card at a time
 for (var i = 0; i <= arrUserNames.length-1; i++) {
-    createCard(arrUserNames[i],arrComments[i],arrDates[i],arrSrcs[i]);
+    createCard(arrUserNames[i],arrComments[i],arrDates[i]);
 }
 
+//need to change the event listener to the entire form, not just the button
 const commentButton = document.querySelector('.comments__button');
 commentButton.addEventListener("click", function(event){
     event.preventDefault()
 });
 
+//event handler that actually posts the comment on the click of the button
 commentButton.onclick = function clickHandler () {
+
+    //grabs the value in the input fields on the website
     var username = document.querySelector(".comments__username").value;
     var comment = document.querySelector(".comments__text").value;
+
+    //aborts execution if the comment or username input is empty
+    if (username=== "" || comment === "") {
+        return alert('Please enter a username');
+    }
     
+    //adds the grabbed values to the beginning of the array
     arrUserNames.unshift(username);
     arrComments.unshift(comment);
     arrDates.unshift('Now');
@@ -62,26 +74,51 @@ commentButton.onclick = function clickHandler () {
     console.log(arrDates);
     console.log(arrSrcs);
 
+    //clears the output section to provide a clean slate for reproducing all elements with the new data
     commentsOutputs.innerHTML = "";
 
+    //iterates through each array and executes the createCard function from before
     for (var i = 0; i <= arrUserNames.length-1; i++) {
         createCard(arrUserNames[i],arrComments[i],arrDates[i],arrSrcs[i]);
     }
 
+    //clears the form after submission
     document.querySelector(".comments__form").reset()
 }
 
 //acquiring an api key
 const apiKeyRequest = axios.get ('https://project-1-api.herokuapp.com/register');
-api_key.then(result => {
-    console.log(result);
-});
 
-//storing the API key, that I grabbed from the browser, in a variable
+//storing the API key that I grabbed from the browser, in a variable
 const apiKey = "234dfbdf-20c1-4168-b9b3-08ec038c0e1a";
+const keyAffix = "?api_key=" + apiKey;
 
 //retrieving comments
-const comments = axios.get (`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`);
+const comments = axios.get ("https://project-1-api.herokuapp.com/comments" + keyAffix);
+
+var arrNewUserNames = [];
+var arrNewComments = [];
+
+console.log(comments)
+
+comments.then(result => {
+    console.log(result)
+    result.data.children.forEach (innerResult => {
+        // console.log(innerResult)
+        arrNewUserNames.push(result.data.name)
+    })
+    result
+})
+
+            
+
+
+
+
+
+    
+
+
 
 
 
--- sprint-3/js/shows.js
+++ sprint-3/js/shows.js
@@ -1,3 +1,4 @@
+//creating and appending the section and section title 
 const showsSection = document.querySelector('.shows');
 
 const title = document.createElement('h2');
@@ -9,54 +10,53 @@ const eventsContainer = document.createElement('div');
 eventsContainer.classList.add('shows__eventscontainer');
 showsSection.appendChild(eventsContainer);
 
-let arrDate = ['Mon Dec 17 2018','Tue Jul 18 2019','Fri Jul 22 2019','Sat Aug 12 2019','Fri Sep 05 2019','Wed Aug 11 2019'];
+const apiKey = "234dfbdf-20c1-4168-b9b3-08ec038c0e1a";
+const keyAffix = "?api_key=" + apiKey;
 
-let arrVenue = ['Ronald Lane','Pier 3 East', 'View Loungue', 'Hyatt Agency','Moscow Center','Pres Club'];
+//retrieving shows dates
+const showDates = axios.get ("https://project-1-api.herokuapp.com/showdates" + keyAffix);
+showDates.then(result => {
+    // console.log(result.data);
+    result.data.forEach (thing => {
 
-const createCard = function (arrDate,arrVenue) {
+        const showsEvent = document.createElement('div');
+        showsEvent.classList.add('shows__event');
+        eventsContainer.appendChild(showsEvent);
 
-    const showsEvent = document.createElement('div');
-    showsEvent.classList.add('shows__event');
-    eventsContainer.appendChild(showsEvent);
+        var showsDesc = document.createElement('h3');
+        showsDesc.classList.add('shows__description');
+        showsDesc.textContent = ('DATES');
+        showsEvent.appendChild(showsDesc);
 
-    var showsDesc = document.createElement('h3');
-    showsDesc.classList.add('shows__description');
-    showsDesc.textContent = ('DATES')
-    showsEvent.appendChild(showsDesc);
+        var showsDetail= document.createElement('h3');
+        showsDetail.classList.add('shows__description--detail');
+        showsDetail.textContent = (thing.date);
+        showsEvent.appendChild(showsDetail);
 
-    var showsDetail= document.createElement('h3');
-    showsDetail.classList.add('shows__description--detail');
-    showsDetail.textContent = (arrDate[i]);
-    showsEvent.appendChild(showsDetail);
+        showsDesc = document.createElement('h3');
+        showsDesc.classList.add('shows__description');
+        showsDesc.textContent = ('VENUE')
+        showsEvent.appendChild(showsDesc);
 
-    showsDesc = document.createElement('h3');
-    showsDesc.classList.add('shows__description');
-    showsDesc.textContent = ('VENUE')
-    showsEvent.appendChild(showsDesc);
+        showsDetail= document.createElement('h3');
+        showsDetail.classList.add('shows__description--detail');
+        showsDetail.textContent = (thing.place);
+        showsEvent.appendChild(showsDetail);
 
-    showsDetail= document.createElement('h3');
-    showsDetail.classList.add('shows__description--detail');
-    showsDetail.textContent = (arrVenue[i]);
-    showsEvent.appendChild(showsDetail);
+        showsDesc = document.createElement('h3');
+        showsDesc.classList.add('shows__description');
+        showsDesc.textContent = ('LOCATION')
+        showsEvent.appendChild(showsDesc);
 
-    showsDesc = document.createElement('h3');
-    showsDesc.classList.add('shows__description');
-    showsDesc.textContent = ('LOCATION')
-    showsEvent.appendChild(showsDesc);
+        showsDetail= document.createElement('h3');
+        showsDetail.classList.add('shows__description--detail');
+        showsDetail.textContent = (thing.location);
+        showsEvent.appendChild(showsDetail);
 
-    showsDetail= document.createElement('h3');
-    showsDetail.classList.add('shows__description--detail');
-    showsDetail.textContent = ('San Fancisco, CA')
-    showsEvent.appendChild(showsDetail);
-
-    var ticketsButton=document.createElement('button');
-    ticketsButton.classList.add('shows__button');
-    ticketsButton.innerHTML = ('BUY TICKETS')
-    showsEvent.appendChild(ticketsButton);
-}
-
-for (var i = 0; i <= 5; i++) {
-    createCard(arrDate,arrVenue);
-}
-
-
+        var ticketsButton=document.createElement('button');
+        ticketsButton.classList.add('shows__button');
+        ticketsButton.innerHTML = ('BUY TICKETS')
+        showsEvent.appendChild(ticketsButton);
+            
+    })
+})
