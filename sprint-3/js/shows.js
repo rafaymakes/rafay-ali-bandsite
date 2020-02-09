f207f31bfcf2482125b98701415874770dab0bf4
Rafay Ali <rafayny@gmail.com>
2020-02-09 03:26:06 -0500

    added the titles to the shows section at the tablet breakpoint

--- sprint-3/js/shows.js
+++ sprint-3/js/shows.js
@@ -1,4 +1,4 @@
-//creating and appending the section and section title 
+//creating and appending the section, section title, and date/venue/location titles
 const showsSection = document.querySelector('.shows');
 
 const title = document.createElement('h2');
@@ -10,6 +10,30 @@ const eventsContainer = document.createElement('div');
 eventsContainer.classList.add('shows__eventscontainer');
 showsSection.appendChild(eventsContainer);
 
+const eventsTitles = document.createElement('div');
+eventsTitles.classList.add('shows__event--titles')
+eventsContainer.appendChild(eventsTitles);
+
+const dateTitle = document.createElement('h3');
+dateTitle.classList.add('shows__description--tablet');
+dateTitle.textContent = ('DATES');
+eventsTitles.appendChild(dateTitle);
+
+const venueTitle = document.createElement('h3');
+venueTitle.classList.add('shows__description--tablet')
+venueTitle.textContent = ('VENUE');
+eventsTitles.appendChild(venueTitle);
+
+const locationTitle = document.createElement('h3');
+locationTitle.classList.add('shows__description--tablet')
+locationTitle.textContent = ('LOCATION');
+eventsTitles.appendChild(locationTitle);
+
+//this hidden button is used to keep the titles flexbox in line with the actual 
+var hiddenDiv=document.createElement('div');
+hiddenDiv.classList.add('shows__hidden');
+eventsTitles.appendChild(hiddenDiv);
+
 //saving the api key and key affix in one variable for easy re-use
 const apiKey = "234dfbdf-20c1-4168-b9b3-08ec038c0e1a";
 const keyAffix = "?api_key=" + apiKey;
@@ -19,7 +43,7 @@ const showDates = axios.get ("https://project-1-api.herokuapp.com/showdates" + k
 showDates.then(result => {
 
     result.data.forEach (thing => {
-        
+
         const showsEvent = document.createElement('div');
         showsEvent.classList.add('shows__event');
         eventsContainer.appendChild(showsEvent);
@@ -31,6 +55,7 @@ showDates.then(result => {
 
         var showsDetail= document.createElement('h3');
         showsDetail.classList.add('shows__description--detail');
+        showsDetail.classList.add('shows__description--detail--date');
         showsDetail.textContent = (thing.date);
         showsEvent.appendChild(showsDetail);
 
